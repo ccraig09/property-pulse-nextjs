a7dc9c5cb41c8847333df0aee96e1bf105056355
ccraig09 <carlos.craig09@gmail.com>
2025-03-09 19:10:07 -0500

    feat: add NavBar to layout and clean up properties page

--- app/properties/[...id]/page.jsx
+++ app/properties/[...id]/page.jsx
@@ -1,14 +1,5 @@
-"use client";
-import { useRouter } from "next/navigation";
-
-const PropertyPage = () => {
-  const router = useRouter();
-  console.log(router);
-  return (
-    <div>
-      <button onClick={() => router.replace("/")}>Go Home</button>
-    </div>
-  );
+const PropertyPage = ({ params }) => {
+  return <div>Property Page {params.id}</div>;
 };
 
 export default PropertyPage;
